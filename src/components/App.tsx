6a46059fd418dee68e89626bd27a2e2104895a47
Jakub Vlasák <vlasakjakub@gmail.com>
2021-12-30 08:36:37 +0100

    Remove lorem ipsum

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -13,12 +13,6 @@ const App: React.FC = () => {
       </header>
       <section className={styles.body}>
         <DashboardPage />
-        <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Eveniet,
-          mollitia at minus incidunt ipsum ullam quae veniam sunt nesciunt vero
-          facilis doloribus reiciendis officiis. Quia dolor debitis magni non
-          quis?
-        </p>
       </section>
       <footer className={styles.footer}>
         <Footer />
