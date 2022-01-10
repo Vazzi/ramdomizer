fcb8b3b22e354392f8c9142e8ff6617dc8f3c5cf
Jakub Vlasák <vlasakjakub@gmail.com>
2022-01-10 17:09:56 +0100

    Random function for random number in utils

--- src/utils/randomFunctions.ts
+++ src/utils/randomFunctions.ts
@@ -1,9 +1,20 @@
+import { Range } from "./types";
+
 /**
  * Get the random YES or NO string
  *
  * @returns YES or NO string
  */
-export const getRandYesNo: () => "YES" | "NO" = () => {
+export const getRandYesNo = ():'YES' | 'NO'  => {
   const rand = Math.round(Math.random());
   return rand ? 'YES' : 'NO';
-}
+};
+
+/**
+ * Get the random number in range
+ *
+ * @returns {number} random integer
+ */
+export const getRandNumber = (range: Range):number => {
+  return Math.round(Math.random() * (range.max - range.min) + range.min);
+};
