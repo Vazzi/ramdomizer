4aaeb891ac177cbc3a98b9521d560b1d9d92f57e
Jakub Vlasák <vlasakjakub@gmail.com>
2022-01-16 14:52:08 +0100

    Random function to pick item from list

--- src/utils/randomFunctions.ts
+++ src/utils/randomFunctions.ts
@@ -1,11 +1,11 @@
-import { Range } from "../models/types";
+import { Range } from '../models/types';
 
 /**
  * Get the random YES or NO string
  *
  * @returns YES or NO string
  */
-export const getRandYesNo = ():'YES' | 'NO'  => {
+export const getRandYesNo = (): 'YES' | 'NO' => {
   const rand = Math.round(Math.random());
   return rand ? 'YES' : 'NO';
 };
@@ -13,8 +13,21 @@ export const getRandYesNo = ():'YES' | 'NO'  => {
 /**
  * Get the random number in range
  *
+ * @param {Range} Range from min to max
  * @returns {number} random integer
  */
-export const getRandNumber = (range: Range):number => {
+export const getRandNumber = (range: Range): number => {
   return Math.round(Math.random() * (range.max - range.min) + range.min);
+};
+
+/**
+ * Get the random item from given list
+ *
+ * @param {T[]} list of items to pick from
+ * @returns {T} randomly pick item
+ */
+export const getRandomItem = <T>(list: T[]): T => {
+  const range: Range = { min: 0, max: list.length - 1 };
+  const index: number = getRandNumber(range);
+  return list[index];
 };
