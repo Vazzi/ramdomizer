024e12c1ab811c12f0c22974f4f203ed3060f39b
Jakub Vlasák <vlasakjakub@gmail.com>
2022-01-16 15:39:20 +0100

    Order list page component implemented

--- src/components/FunctionPages/OrderListPage/OrderListPage.tsx
+++ src/components/FunctionPages/OrderListPage/OrderListPage.tsx
@@ -1,11 +1,41 @@
 import React from 'react';
 
+import BaseFunctionPage from '../BaseFunctionPage/BaseFunctionPage';
+import styles from './OrderListPage.module.scss';
+import { removeEmptyItems } from '../../../utils/list';
+import { setRandomOrder } from '../../../utils/randomFunctions';
+
 const OrderListPage: React.FC = () => {
+  const [list, setList] = React.useState<string[] | null>(null);
+  const inputRef = React.useRef<HTMLTextAreaElement>(null);
+
+  const handleRoll = () => {
+    const inputText: string = inputRef.current ? inputRef.current.value : '';
+    const listOfItems: string[] = removeEmptyItems(inputText.split('\n'));
+    const orderedList: string[] = setRandomOrder(listOfItems);
+    setList(orderedList);
+  };
+
+  const resultEl = list ? (
+    <ol className={styles.list}>
+      {list.map((item, i) => (
+        <li key={i}>{item}</li>
+      ))}
+    </ol>
+  ) : null;
+
   return (
-    <>
-      <h2>OrderListPage</h2>
-      <p>Work in progress...</p>
-    </>
+    <BaseFunctionPage
+      title="Randomly order list"
+      description="Randomly order the given list of items. "
+      onRoll={handleRoll}
+      result={resultEl}
+    >
+      <div className={styles.form}>
+        <textarea rows={10} id="input-list" ref={inputRef} />
+        <label>Insert your list. Each item on one line.</label>
+      </div>
+    </BaseFunctionPage>
   );
 };
 
