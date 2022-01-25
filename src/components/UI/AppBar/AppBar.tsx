0f68661c1fa88f0fffd75bbf44afb1d82021c39a
Jakub Vlasák <vlasakjakub@gmail.com>
2022-01-25 17:03:06 +0100

    Dice in appbar

--- src/components/UI/AppBar/AppBar.tsx
+++ src/components/UI/AppBar/AppBar.tsx
@@ -2,6 +2,8 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 import styles from './AppBar.module.scss';
+import Dice from '../Dice/Dice';
+import { getRandDiceRoll } from '../../../utils/randomFunctions';
 
 /**
  * AppBar
@@ -12,6 +14,7 @@ import styles from './AppBar.module.scss';
 const AppBar: React.FC = () => {
   return (
     <div className={styles.container}>
+      <Dice value={getRandDiceRoll()} size={20} />
       <Link to="/">
         <h3>Randomizer</h3>
       </Link>
